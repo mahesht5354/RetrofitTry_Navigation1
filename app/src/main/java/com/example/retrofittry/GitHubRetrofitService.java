ad5ca0a044d53c13791a8866589787ff2a480d13
Raj <rntummala47@gmail.com>
2022-09-10 22:37:48 -0500

    lewlf

--- app/src/main/java/com/example/retrofittry/GitHubRetrofitService.java
+++ app/src/main/java/com/example/retrofittry/GitHubRetrofitService.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+
 import retrofit2.http.Query;
 
 public interface GitHubRetrofitService {
 
     @GET("news")
-    Call<List<Source>> getGitHubData(@Query("category") String category);
+    Call<Source> getGitHubData(@Query("category") String category);
 
 
 }
--- app/src/main/java/com/example/retrofittry/MainActivity.java
+++ app/src/main/java/com/example/retrofittry/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.retrofittry;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.retrofittry.GitHubRetrofitService;
+import com.example.retrofittry.Model.Datum;
 import com.example.retrofittry.Model.Source;
 import com.example.retrofittry.R;
+import com.google.android.material.tabs.TabLayout;
+import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -21,72 +37,112 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView textView;
-    ProgressBar progressBar;
+    TabLayout tl;
+    ViewPager vp;
+    RecyclerView recyclerView;
+    Button b;
     Retrofit retrofit;
     GitHubRetrofitService gfs;
+    Gson gson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //recyclerView = findViewById(R.id.recyclerView);
+        tl = findViewById(R.id.tabLayout);
+        vp = findViewById(R.id.viewpager);
+        Vpadapter vpadapter = new Vpadapter(getSupportFragmentManager());
+        vp.setAdapter(vpadapter);
+        tl.setupWithViewPager(vp);
+        b = findViewById(R.id.button);
         retrofit = new Retrofit.Builder()
                 .baseUrl("https://inshortsapi.vercel.app/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         gfs = retrofit.create(GitHubRetrofitService.class);
-        textView = findViewById(R.id.textView);
-        progressBar = findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.INVISIBLE);
+        b.setVisibility(View.GONE);
+
+        List<News> list = new ArrayList<>();
+        Call<Source> call = gfs.getGitHubData("entertainment");
+        call.enqueue(new Callback<Source>() {
+            @Override
+            public void onResponse(Call<Source> call, Response<Source> response) {
+
+
+                for (int i=0; i<response.body().getData().size();i++)
+                {
+                    String title = response.body().getData().get(i).getTitle();
+                    String content = response.body().getData().get(i).getContent();
+                    String imageUrl = response.body().getData().get(i).getImageUrl();
+
+                    //Toast.makeText(MainActivity.this, title+"\n"+content+"\n"+imageUrl, Toast.LENGTH_LONG).show();
+                    list.add(new News(title, content, imageUrl));
+                }
+                String jsonList = gson.toJson(list);
+
+
+                RedFragment fragment = new RedFragment(); // replace your custom fragment class
+                Bundle bundle = new Bundle();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                bundle.putString("LIST",jsonList); // use as per your need
+                fragment.setArguments(bundle);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                /*NewsAdapter newsAdapter = new NewsAdapter(MainActivity.this, list);
+                recyclerView.setAdapter(newsAdapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));*/
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Source> call, Throwable t) {
+
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 
     public void getDeatils(View view) {
 
-        progressBar.setVisibility(View.VISIBLE);
-        Call<List<Source>> call = gfs.getGitHubData("sports");
-        call.enqueue(new Callback<List<Source>>() {
-            @Override
-            public void onResponse(Call<List<Source>> call, Response<List<Source>> response) {
-                progressBar.setVisibility(View.INVISIBLE);
-                Toast.makeText(MainActivity.this, "Mahesh", Toast.LENGTH_LONG).show();
+    }
+
+    class Vpadapter extends FragmentStatePagerAdapter
+    {
+
+        public Vpadapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: return new RedFragment();
+                case 1: return new GreenFragment();
+                case 2: return new YellowFragment();
             }
+            return null;
+        }
 
-            @Override
-            public void onFailure(Call<List<Source>> call, Throwable t) {
-                progressBar.setVisibility(View.INVISIBLE);
-                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0: return "Sports";
+                case 1: return "Entertainment";
+                case 2: return "Politics";
             }
-        });
-        /*call.enqueue(new Callback<List<Source>>() {
-            @Override
-            public void onResponse(Call<List<Source>> call, Response<List<Source>> response) {
-                progressBar.setVisibility(View.INVISIBLE);
-                textView.setText("");
-                //Source s = response.body().get(0);
-                for(Source s: response.body())
-
-                {
-                    Toast.makeText(MainActivity.this, s.getCategory().toString(), Toast.LENGTH_LONG).show();
-
-                }
-
-                *//*if (s.getName() == null){
-                    Toast.makeText(MainActivity.this, " name is null",Toast.LENGTH_LONG).show();
-                }
-                else {
-                    textView.append(s.);
-                    Toast.makeText(MainActivity.this, s.getName().toString(), Toast.LENGTH_LONG).show();
-                }*//*
-            }
-
-            @Override
-            public void onFailure(Call<List<Source>> call, Throwable t) {
-                progressBar.setVisibility(View.INVISIBLE);
-                Toast.makeText(MainActivity.this, "Error",Toast.LENGTH_LONG).show();
-            }
-        });*/
-
+            return "null";
+        }
 
 
     }
