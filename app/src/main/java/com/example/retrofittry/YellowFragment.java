f871c3ac7cfc2fea991937fdc1b491190b2cfff9
Raj <rntummala47@gmail.com>
2022-09-12 18:53:24 -0500

    full working code-1

--- app/src/main/java/com/example/retrofittry/YellowFragment.java
+++ app/src/main/java/com/example/retrofittry/YellowFragment.java
@@ -1,11 +1,26 @@
 package com.example.retrofittry;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.retrofittry.Model.APIClient;
+import com.example.retrofittry.Model.Source;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,50 +29,69 @@ import androidx.fragment.app.Fragment;
  */
 public class YellowFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public YellowFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment YellowFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static YellowFragment newInstance(String param1, String param2) {
-        YellowFragment fragment = new YellowFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    GitHubRetrofitService apiInterface;
+    Context context;
+    RecyclerView recyclerView;
+    NewsAdapter newsAdapter;
+    List<News> list;
+    ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_yellow, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_yellow, container, false);
+        // 1. get a reference to recyclerView
+        recyclerView = rootView.findViewById(R.id.recyclerView_yellow);
+        progressBar = rootView.findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        APICall();
+
+        return rootView.getRootView();
+    }
+
+    private void APICall() {
+        apiInterface = APIClient.getClient().create(GitHubRetrofitService.class);
+        //gfs = retrofit.create(GitHubRetrofitService.class);
+        /*LinearLayoutManager manager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setHasFixedSize(true);*/
+
+        //recyclerView.setAdapter(newsAdapter);
+        list = new ArrayList<>();
+
+        Call<Source> call = apiInterface.getGitHubData("politics");
+        call.enqueue(new Callback<Source>() {
+            @Override
+            public void onResponse(Call<Source> call, Response<Source> response) {
+
+                progressBar.setVisibility(View.GONE);
+                for (int i=0; i<response.body().getData().size();i++)
+                {
+                    String title = response.body().getData().get(i).getTitle();
+                    String content = response.body().getData().get(i).getContent();
+                    String imageUrl = response.body().getData().get(i).getImageUrl();
+
+                    //Toast.makeText(MainActivity.this, title+"\n"+content+"\n"+imageUrl, Toast.LENGTH_LONG).show();
+                    list.add(new News(title, content, imageUrl));
+                }
+
+
+                newsAdapter = new NewsAdapter(getContext(), list );
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                recyclerView.setAdapter(newsAdapter);
+
+                //newsAdapter = new NewsAdapter(getContext(), list);
+                newsAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onFailure(Call<Source> call, Throwable t) {
+
+                Toast.makeText(context, "Error", Toast.LENGTH_LONG).show();
+            }
+        });
+        //newsAdapter.notifyDataSetChanged();
     }
 }
