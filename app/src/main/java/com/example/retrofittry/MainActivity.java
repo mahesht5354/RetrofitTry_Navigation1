42c41979ce74be3fb99a1ac985b78ee8c01bf921
Raj <rntummala47@gmail.com>
2022-09-17 22:59:55 -0500

    Adding Navigation Component

--- app/src/main/java/com/example/retrofittry/MainActivity.java
+++ app/src/main/java/com/example/retrofittry/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.retrofittry;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -12,17 +14,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.retrofittry.GitHubRetrofitService;
 import com.example.retrofittry.Model.Datum;
 import com.example.retrofittry.Model.Source;
 import com.example.retrofittry.R;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.gson.Gson;
 
@@ -44,25 +49,109 @@ public class MainActivity extends AppCompatActivity {
     Retrofit retrofit;
     GitHubRetrofitService gfs;
     Gson gson;
+    public DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView nvDrawer;
+    private DrawerLayout mDrawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //recyclerView = findViewById(R.id.recyclerView);
-        tl = findViewById(R.id.tabLayout);
-        vp = findViewById(R.id.viewpager);
-        Vpadapter vpadapter = new Vpadapter(getSupportFragmentManager());
+        /*tl = findViewById(R.id.tabLayout);
+        vp = findViewById(R.id.viewpager);*/
+        /*Vpadapter vpadapter = new Vpadapter(getSupportFragmentManager());
         vp.setAdapter(vpadapter);
-        tl.setupWithViewPager(vp);
-        b = findViewById(R.id.button);
-        b.setVisibility(View.GONE);
+        tl.setupWithViewPager(vp);*/
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        //toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+
+        // This will display an Up icon (<-), we will replace it with hamburger later
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //b = findViewById(R.id.button);
+        //b.setVisibility(View.GONE);
+        mDrawer = (DrawerLayout) findViewById(R.id.my_drawer_layout);
     }
 
-    public void getDeatils(View view) {
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
-    class Vpadapter extends FragmentStatePagerAdapter
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_entertainment:
+                fragmentClass = GreenFragment.class;
+                break;
+            case R.id.nav_politics:
+                fragmentClass = YellowFragment.class;
+                break;
+            default:
+                fragmentClass = RedFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+    }
+
+
+
+
+
+
+    /*class Vpadapter extends FragmentStatePagerAdapter
     {
 
         public Vpadapter(@NonNull FragmentManager fm) {
@@ -97,5 +186,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-    }
+    }*/
 }
